d61a5922c7d8627d79cef81ccd5e4bd8665d97c5
Abdul Hamrah <>
2021-07-30 21:40:17 -0400

    first code transfer

---
+++ src/myrepoPractice/MyrepoTest.java
@@ -0,0 +1,17 @@
+package myrepoPractice;
+
+public class MyrepoTest {
+
+	public static void main(String[] args) {
+		 Double d = new Double("6.5512486728");
+	        
+	        System.out.println("Double to byte: " + d.byteValue() );
+	        System.out.println("Double to short: " + d.shortValue() );
+	        System.out.println("Double to int: " + d.intValue() );
+	        System.out.println("Double to long: " + d.longValue() );
+	        System.out.println("Double to float: " + d.floatValue() );
+	        System.out.println("Double to double: " + d.doubleValue() );        
+	    }  
+	}
+
+
