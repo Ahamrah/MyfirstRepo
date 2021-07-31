517a7fef7454caae671677182ed996dec121729f
Abdul Hamrah <>
2021-07-30 21:45:14 -0400

    just code added

--- src/myrepoPractice/MyrepoTest.java
+++ src/myrepoPractice/MyrepoTest.java
@@ -10,8 +10,20 @@ public class MyrepoTest {
 	        System.out.println("Double to int: " + d.intValue() );
 	        System.out.println("Double to long: " + d.longValue() );
 	        System.out.println("Double to float: " + d.floatValue() );
-	        System.out.println("Double to double: " + d.doubleValue() );        
+	        System.out.println("Double to double: " + d.doubleValue() );     
+	        
+	        
+	        Long l = new Long("81");
+	        
+	        System.out.println("Long to byte: " + l.byteValue() );
+	        System.out.println("Long to short: " + l.shortValue() );
+	        System.out.println("Long to int: " + l.intValue() );
+	        System.out.println("Long to long: " + l.longValue() );
+	        System.out.println("Long to float: " + l.floatValue() );
+	        System.out.println("Long to double: " + l.doubleValue() );
+	    }
+	
 	    }  
-	}
+	
 
 
